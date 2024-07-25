dab9bc37cfce84838503f7e43a26760cc27b05ad
Etienne Millon <me@emillon.org>
2024-07-25 12:48:03 +0200

    :art: do not use tree-sitter ids

--- grammar.js
+++ grammar.js
@@ -1,8 +1,9 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-const stanza = (name, field) => seq("(", name, repeat(field), ")");
-const field = (name, value) => seq("(", name, value, ")");
+const stanza = (name, field_parser) =>
+  seq("(", name, repeat(field_parser), ")");
+const dune_field = (name, value) => seq("(", name, value, ")");
 
 module.exports = grammar({
   name: "dune",
@@ -20,10 +21,11 @@ module.exports = grammar({
         $._field_executable_public_name,
         $._field_executable_libraries,
       ),
-    _field_executable_name: ($) => field("name", $.module_name),
-    _field_executable_public_name: ($) => field("public_name", $.public_name),
+    _field_executable_name: ($) => dune_field("name", $.module_name),
+    _field_executable_public_name: ($) =>
+      dune_field("public_name", $.public_name),
     _field_executable_libraries: ($) =>
-      field("libraries", repeat($.library_name)),
+      dune_field("libraries", repeat($.library_name)),
     library_name: ($) => alias($.atom, "library_name"),
     public_name: ($) => alias($.atom, "public_name"),
     module_name: ($) => alias($.atom, "module_name"),
@@ -36,10 +38,10 @@ module.exports = grammar({
         $.field_rule_action,
         $.sexp,
       ),
-    field_rule_mode: ($) => field("mode", $.sexp),
-    field_rule_target: ($) => field("target", $.atom),
-    field_rule_deps: ($) => field("deps", $.sexp),
-    field_rule_action: ($) => field("action", $.action),
+    field_rule_mode: ($) => dune_field("mode", $.sexp),
+    field_rule_target: ($) => dune_field("target", $.atom),
+    field_rule_deps: ($) => dune_field("deps", $.sexp),
+    field_rule_action: ($) => dune_field("action", $.action),
     action: ($) => $.sexp,
   },
 });
